6e8d3da6de88d463adddda26e9321495476f1000
FelipeET <felipe.etchevers@correo.ucu.edu.uy>
2023-05-01 14:23:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+copynumber.addEventListener("click", numberFunction);
+copygmail.addEventListener("click", gmailFunction);
+copyoutlook.addEventListener("click", outlookFunction);
+
+function numberFunction() {
+    var copied = document.getElementById("copynumber").innerText
+    var input = document.createElement('textarea');
+    input.value = copied;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    showCopiedText();
+}
+
+function gmailFunction() {
+    var copied = document.getElementById("copygmail").innerText
+    var input = document.createElement('textarea');
+    input.value = copied;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    showCopiedText();
+}
+
+function outlookFunction() {
+    var copied = document.getElementById("copyoutlook").innerText
+    var input = document.createElement('textarea');
+    input.value = copied;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    showCopiedText();
+}
+
+function showCopiedText() {
+    var copiedPopUp = document.getElementById("copiedPopUp");
+    copiedPopUp.classList.remove("hiddenPop");
+    copiedPopUp.classList.add("visible");
+    setTimeout(function() {
+      copiedPopUp.classList.remove("visible");
+      copiedPopUp.classList.add("hiddenPop");
+    }, 3000);
+  }
+
+  
